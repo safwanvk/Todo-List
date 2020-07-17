f6f166c4404c4f71040d90a04c20f11dc6580ba6
safwanvk <safwanvalakundil@gmail.com>
2020-07-17 17:40:57 +0530

    :fire: Remove serializers

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -1,9 +1,1 @@
-from rest_framework import serializers
 
-from .models import Hero
-
-
-class HeroSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Hero
-        fields = ('name', 'alias')
